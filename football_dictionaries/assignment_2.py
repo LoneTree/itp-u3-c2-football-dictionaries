e2c78f3cd3769ffd04743af13f0f63e23312d774
Brandon <brandon.j.silva@gmail.com>
2020-06-09 15:26:54 -0700

    Passed PyTest
    Passed 3

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,18 @@
+# Data
+from football_dictionaries.squads_data import SQUADS_DATA
+
+# Assignment 1
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+
+    result = {}
+
+    squads_dict = players_as_dictionaries(squads_list)
+
+    for player in squads_dict:
+        position = player['position']
+        result.setdefault(position, [])
+        result[position].append(player)
+
+    return result
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,23 @@
+# Data
+from football_dictionaries.squads_data import SQUADS_DATA
+
+# Assignment 1
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
-    pass
+
+    result = {}
+
+    squads_dict = players_as_dictionaries(squads_list)
+
+    for player in squads_dict:
+        position = player['position']
+        country = player['country']
+        result.setdefault(country, {})
+
+        result[country].setdefault(position, [])
+        result[country][position].append(player)
+
+    return result
+
+
